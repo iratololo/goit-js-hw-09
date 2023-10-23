dc2a7e22a730b71dda8b9f19640a7e5cdb34d9e1
iratololo <iratololo1991@gmail.com>
2023-10-23 18:26:54 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const selectors = {
+    startButton: document.querySelector("button[data-start]"),
+    stopButton: document.querySelector("button[data-stop]"),
+    body: document.querySelector("body")
+}
+
+document.addEventListener("click", changeкBackground);
+
+let timerId = null;
+
+function changeкBackground(evt) {
+    if (evt.target === selectors.startButton) {
+        evt.target.disabled = true;
+        selectors.stopButton.disabled = false;
+        timerId = setInterval(() => {
+          selectors.body.style.backgroundColor = getRandomHexColor();  
+        },1000);
+    }
+    if (evt.target === selectors.stopButton) {
+        evt.target.disabled = true;
+        selectors.startButton.disabled = false;
+        clearTimeout(timerId);
+    }
+};
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
 
