a35dc72b2261cc33c4bf0e39147a5bc3878dd394
iratololo <iratololo1991@gmail.com>
2023-10-25 23:06:58 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", handlerForm);
+
+
+function handlerForm(evt) {
+  evt.preventDefault();
+let delay = Number(evt.currentTarget.elements.delay.value);
+let step = Number(evt.currentTarget.elements.step.value);
+  const amount = Number(evt.currentTarget.elements.amount.value);
+  for (let i = 1; i <= amount; i++) {
+    const promise = createPromise(i, delay);
+    console.log(promise);
+    promise.then(({position, delay}) => {
+      Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    }).catch(({position, delay}) => {
+      Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    }) 
+    delay += step;
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({
+          position: position,
+          delay: delay,
+        });
+  } else {
+        reject({
+          position: position,
+          delay: delay,
+        });
+      }
+    }, delay);
+  })
+}
+
