2012476b2014dabc4cd24b5c0b070e6cf84b307b
iratololo <iratololo1991@gmail.com>
2023-10-25 16:21:13 +0300

    task2

---
+++ src/js/02-timer.js
@@ -0,0 +1,114 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+const selectors = {
+    container: document.querySelector("body"),
+    timerButton: document.querySelector("button[data-start]"),
+    timerButtonStop: document.querySelector("button[data-stop]"),
+    days: document.querySelector("span[data-days]"),
+    hours: document.querySelector("span[data-hours]"),
+    minutes: document.querySelector("span[data-minutes]"),
+    seconds: document.querySelector("span[data-seconds]"),
+}
+
+selectors.timerButton.disabled = true;
+selectors.container.style.backgroundColor = '#ffd54f';
+selectors.timerButton.addEventListener("click", handlerTimer);
+
+const timerInterval = 1000;
+
+let result = 0;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        const selectedDate = selectedDates[0].getTime();
+        const defaultDate = options.defaultDate.getTime();
+        result = selectedDate - defaultDate;
+        if (selectedDate < defaultDate) {
+        Report.failure(
+            'Invalid date',
+            'Please choose a date in the future',
+            'Okay',
+            );
+        } else {
+            selectors.timerButton.disabled = false;
+        }
+    },
+};
+
+flatpickr("#datetime-picker", options);
+
+function handlerTimer() {
+    selectors.timerButton.disabled = true;
+    const timerId = setInterval(() => {
+        result -= timerInterval;
+        const timeData = convertMs(result);
+        const { days, hours, minutes, seconds } = timeData;
+        // console.log('result',result);
+        selectors.days.textContent = addLeadingZero(days);
+        selectors.hours.textContent = addLeadingZero(hours);
+        selectors.minutes.textContent = addLeadingZero(minutes);
+        selectors.seconds.textContent = addLeadingZero(seconds);
+       if (result < timerInterval) {
+           clearInterval(timerId);
+           Report.success(
+            'Time is over 🎉',
+            'Please choose new date',
+            'Okay',
+           );
+           selectors.days.textContent = addLeadingZero(0);
+            selectors.hours.textContent = addLeadingZero(0);
+            selectors.minutes.textContent = addLeadingZero(0);
+            selectors.seconds.textContent = addLeadingZero(0);
+        }
+   }, timerInterval)
+}
+
+
+function addLeadingZero(value) {
+   return value.toString().padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+    
+
+
+
+// setInterval(() => {
+//     const today = new Date();
+//     const tomorrow = new Date("December 16, 2023");
+//     const timeDefoult = today.getTime();
+//     const tomorrowDefoult = tomorrow.getTime();
+//     const result = tomorrowDefoult - timeDefoult;
+//     const timeData = convertMs(result);
+//     console.log('timeData',timeData);
+//     const { days, hours, minutes, seconds } = timeData;
+
+//     selectors.days.textContent = addLeadingZero(days);
+//     selectors.hours.textContent = addLeadingZero(hours);
+//     selectors.minutes.textContent = addLeadingZero(minutes);
+//     selectors.seconds.textContent = addLeadingZero(seconds);
+// },1000)
